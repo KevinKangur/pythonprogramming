a1ff66f561361d78fc32cadc7055609a99fd1cca
KevinKangur <kevinkangur13@gmail.com>
2022-10-11 13:24:24 +0300

    Initial commit

---
+++ yl2.py
@@ -0,0 +1,11 @@
+import math
+
+r = int(input("Kui suur on ringi raadius sentimeetrites? "))
+
+p = math.pi * r * r
+
+print("Ringi pindala on ", p, "ruutsentimeetrit")
+
+c = 2 * math.pi * r
+
+print("Ringi ümbermõõt on ", c, "sentimeetrit")
