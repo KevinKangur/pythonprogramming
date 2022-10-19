9df597da544e20c746a15b0dd54429ca3da71997
KevinKangur <kevinkangur13@gmail.com>
2022-10-19 09:51:49 +0300

    Initial commit

--- yl3.py
+++ yl3.py
@@ -1,2 +1,9 @@
-n = int(input("Sisestage üks täisarv 1-9 "))
+n = input("Täisarv vhaemikus 1-9: ")
 
+nn = n + n
+
+nnn = n + n + n
+
+res = int(n) + int(nn) + int(nnn)
+
+print(n, "+", nn, "+", nnn, "=", res)
