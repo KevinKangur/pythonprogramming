d05c258007ac9bd4b7e6c4906542ccdf866f81ee
KevinKangur <kevinkangur13@gmail.com>
2022-11-10 11:36:42 +0200

    first commit

---
+++ yl13.py
@@ -0,0 +1,12 @@
+pet = input("Mis lemmikloom teil on? - ")
+
+if pet.lower() == "koer":
+    print("What the dog doin?")
+    print(pet[0])
+else:
+    print(pet[0])
+
+petlist = ["Hamster", "Kuldkala", "Hiir", pet]
+
+print(petlist)
+print(pet[-1])
