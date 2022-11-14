333198168da8f29f5f109cadaaf1a727fc9f1796
KevinKangur <kevinkangur13@gmail.com>
2022-11-14 11:36:02 +0200

    first commit

--- yl11.py
+++ yl11.py
@@ -1,1 +1,12 @@
-text = input("Sisend: ")
+import math
+
+text = input("Sisend: ").strip()
+
+print(len(text))
+
+if len(text) >= 7 and (len(text) % 2) == 1:
+    print("Sisend vastab tingimustele.")
+    i = math.floor(len(text) / 2)
+    print(text[i-1:i+2])
+else:
+    print("Sisend ei vasta tingimustele!")
