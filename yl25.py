aa2d1d778de1468deedefd141a375e1da8928397
KevinKangur <kevinkangur13@gmail.com>
2023-01-19 18:23:04 +0200

    yl25 finished

--- yl25.py
+++ yl25.py
@@ -1,4 +1,4 @@
-thisdict = {
+mina = {
     'eesnimi': 'Kevin',
     'perenimi': 'Kangur',
     'sünniaasta': '2005',
@@ -6,10 +6,30 @@ thisdict = {
     'lemmik magustoit': 'riisi puder kisseliga'
 }
 
-print(thisdict.get('elukoht'))
-print(thisdict['elukoht'])
+print(mina.get('elukoht'))
+print(mina['elukoht'])
 
-thisdict['lemmik magustoit'] = 'tarretis'
+mina['lemmik magustoit'] = 'tarretis'
 
-for k, v in thisdict.items():
+for k, v in mina.items():
     print(k, v)
+
+if 'isikukood' in mina:
+    print('isikukood on olemas')
+else:
+    print('isikukood puudub')
+
+print(len(mina))
+
+mina['pikkus'] = '180 cm'
+print(mina)
+
+#print(mina.pop('sünniaasta'))
+del mina['sünniaasta']
+print(mina)
+
+mina.clear()
+print(mina)
+
+del mina
+print(mina)
