4144824129fdb06d03471f18ae8eb90903a663f0
KevinKangur <kevinkangur13@gmail.com>
2023-01-20 11:47:50 +0200

    yl26 input data start

---
+++ yl26_input_data.py
@@ -0,0 +1,16 @@
+sales_results = {
+    'Johnver': {
+        'revenue': {
+            'tea': int(190),
+            'coffee': int(325),
+            'water': int(625),
+            'milk': int(829),
+        },
+        'expenses': {
+            'tea': '120',
+            'coffee': '300',
+            'water': '50',
+            'milk': '67',
+        }
+    },
+}
