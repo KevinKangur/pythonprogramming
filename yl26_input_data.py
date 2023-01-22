29b1704d6c9cfd1e9246c327bdf45389c75643a3
KevinKangur <kevinkangur13@gmail.com>
2023-01-22 16:58:12 +0200

    finished input data

--- yl26_input_data.py
+++ yl26_input_data.py
@@ -7,10 +7,70 @@ sales_results = {
             'milk': int(829),
         },
         'expenses': {
-            'tea': '120',
-            'coffee': '300',
-            'water': '50',
-            'milk': '67',
+            'tea': int(120),
+            'coffee': int(300),
+            'water': int(50),
+            'milk': int(67),
+        }
+    },
+
+    'Vanston': {
+        'revenue': {
+            'tea': int(140),
+            'coffee': int(19),
+            'water': int(14),
+            'milk': int(140),
+        },
+        'expenses': {
+            'tea': int(65),
+            'coffee': int(10),
+            'water': int(299),
+            'milk': int(254),
+        }
+    },
+
+    'Danbree': {
+        'revenue': {
+            'tea': int(1956),
+            'coffee': int(293),
+            'water': int(852),
+            'milk': int(609),
+        },
+        'expenses': {
+            'tea': int(890),
+            'coffee': int(23),
+            'water': int(1290),
+            'milk': int(89),
+        }
+    },
+
+    'Vansey': {
+        'revenue': {
+            'tea': int(14),
+            'coffee': int(1491),
+            'water': int(56),
+            'milk': int(120),
+        },
+        'expenses': {
+            'tea': int(54),
+            'coffee': int(802),
+            'water': int(12),
+            'milk': int(129),
+        }
+    },
+
+    'Mundyke': {
+        'revenue': {
+            'tea': int(143),
+            'coffee': int(162),
+            'water': int(659),
+            'milk': int(87),
+        },
+        'expenses': {
+            'tea': int(430),
+            'coffee': int(235),
+            'water': int(145),
+            'milk': int(76),
         }
     },
 }
