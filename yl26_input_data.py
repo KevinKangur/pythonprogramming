89939beb176a42da85e6ba40a377e7a0552bf5b4
KevinKangur <kevinkangur13@gmail.com>
2023-01-23 09:13:50 +0200

    yl26 input data fixed

--- yl26_input_data.py
+++ yl26_input_data.py
@@ -1,76 +1,76 @@
 sales_results = {
     'Johnver': {
         'revenue': {
-            'tea': int(190),
-            'coffee': int(325),
-            'water': int(625),
-            'milk': int(829),
+            'tea': 190,
+            'coffee': 325,
+            'water': 625,
+            'milk': 829,
         },
         'expenses': {
-            'tea': int(120),
-            'coffee': int(300),
-            'water': int(50),
-            'milk': int(67),
+            'tea': 120,
+            'coffee': 300,
+            'water': 50,
+            'milk': 67,
         }
     },
 
     'Vanston': {
         'revenue': {
-            'tea': int(140),
-            'coffee': int(19),
-            'water': int(14),
-            'milk': int(140),
+            'tea': 140,
+            'coffee': 19,
+            'water': 14,
+            'milk': 140,
         },
         'expenses': {
-            'tea': int(65),
-            'coffee': int(10),
-            'water': int(299),
-            'milk': int(254),
+            'tea': 65,
+            'coffee': 10,
+            'water': 299,
+            'milk': 254,
         }
     },
 
     'Danbree': {
         'revenue': {
-            'tea': int(1956),
-            'coffee': int(293),
-            'water': int(852),
-            'milk': int(609),
+            'tea': 1956,
+            'coffee': 293,
+            'water': 852,
+            'milk': 609,
         },
         'expenses': {
-            'tea': int(890),
-            'coffee': int(23),
-            'water': int(1290),
-            'milk': int(89),
+            'tea': 890,
+            'coffee': 23,
+            'water': 1290,
+            'milk': 89,
         }
     },
 
     'Vansey': {
         'revenue': {
-            'tea': int(14),
-            'coffee': int(1491),
-            'water': int(56),
-            'milk': int(120),
+            'tea': 14,
+            'coffee': 1491,
+            'water': 56,
+            'milk': 120,
         },
         'expenses': {
-            'tea': int(54),
-            'coffee': int(802),
-            'water': int(12),
-            'milk': int(129),
+            'tea': 54,
+            'coffee': 802,
+            'water': 12,
+            'milk': 129,
         }
     },
 
     'Mundyke': {
         'revenue': {
-            'tea': int(143),
-            'coffee': int(162),
-            'water': int(659),
-            'milk': int(87),
+            'tea': 143,
+            'coffee': 162,
+            'water': 659,
+            'milk': 87,
         },
         'expenses': {
-            'tea': int(430),
-            'coffee': int(235),
-            'water': int(145),
-            'milk': int(76),
+            'tea': 430,
+            'coffee': 235,
+            'water': 145,
+            'milk': 76,
         }
     },
 }
